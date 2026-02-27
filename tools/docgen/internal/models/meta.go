d220033c57c1d32419092113914014fb72ea3977
winerty <nikitokmay@gmail.com>
2026-02-27 13:38:57 +0300

    Upd: docgen tags for build jsonю Feat: new docs for mark-service

--- tools/docgen/internal/models/meta.go
+++ tools/docgen/internal/models/meta.go
@@ -5,21 +5,21 @@ package models
 // Техническая информация
 
 type Meta struct {
-	Name           string         `yaml:"name"`
-	DisplayName    string         `yaml:"displayName"`
-	Description    string         `yaml:"description"`
-	Version        string         `yaml:"version"`
-	Struct         Struct         `yaml:"struct"`
-	Infrastructure Infrastructure `yaml:"infrastructure"`
-	Tags           []string       `yaml:"tags"`
+	Name           string         `yaml:"name" json:"name"`
+	DisplayName    string         `yaml:"displayName" json:"displayName"`
+	Description    string         `yaml:"description" json:"description"`
+	Version        string         `yaml:"version" json:"version"`
+	Struct         Struct         `yaml:"struct" json:"struct"`
+	Infrastructure Infrastructure `yaml:"infrastructure" json:"infrastructure"`
+	Tags           []string       `yaml:"tags" json:"tags"`
 }
 
 type Struct struct {
-	Language  string   `yaml:"language"`
-	Protocols []string `yaml:"protocols"`
+	Language  string   `yaml:"language" json:"language"`
+	Protocols []string `yaml:"protocols" json:"protocols"`
 }
 
 type Infrastructure struct {
-	Type        []string `yaml:"type"`
-	Description []string `yaml:"description"`
+	Type        []string `yaml:"type" json:"type"`
+	Description []string `yaml:"description" json:"description"`
 }
