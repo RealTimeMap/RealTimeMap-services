b1f80d81719c563fb4375d1e7e7c975c48896f8f
winerty <nikitokmay@gmail.com>
2026-05-02 19:38:18 +0300

    Upd(comment-service): integrate new health handler + traefik setting for this

--- services/comment-service/cmd/comment-service/main.go
+++ services/comment-service/cmd/comment-service/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/RealTimeMap/RealTimeMap-backend/pkg/database"
 	"github.com/RealTimeMap/RealTimeMap-backend/pkg/logger"
+	"github.com/RealTimeMap/RealTimeMap-backend/pkg/runner"
+	httpserver "github.com/RealTimeMap/RealTimeMap-backend/pkg/transport/http"
 	"github.com/RealTimeMap/RealTimeMap-backend/services/comment-service/internal/app"
 	"github.com/RealTimeMap/RealTimeMap-backend/services/comment-service/internal/config"
 	"github.com/RealTimeMap/RealTimeMap-backend/services/comment-service/internal/domain/model"
 	httptransport "github.com/RealTimeMap/RealTimeMap-backend/services/comment-service/internal/transport/http"
 	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -37,33 +33,11 @@ func main() {
 	container := app.NewContainer(cfg, db, log)
 	defer container.Close()
 
-	httpServer := httptransport.NewServer(cfg.HTTP.Port, log)
+	httpServer := httpserver.NewServer(cfg.HTTP, log)
 	httptransport.RegisterRoutes(httpServer.Router(), container)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	go func() {
-		sigCh := make(chan os.Signal, 1)
-		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-		<-sigCh
-		log.Info("Shutdown signal received")
-		cancel()
-	}()
-
-	// Run all servers with errgroup
-	g, gCtx := errgroup.WithContext(ctx)
-
-	g.Go(func() error {
-		return httpServer.Run()
-	})
-	g.Go(func() error {
-		<-gCtx.Done()
-		return httpServer.Shutdown(context.Background())
-	})
-
-	if err := g.Wait(); err != nil {
-		log.Error("Server error", zap.Error(err))
+	if err := runner.Run(log, httpServer); err != nil {
+		log.Fatal("Comment Service error", zap.Error(err))
 	}
 
 	log.Info("Comment Service stopped")
