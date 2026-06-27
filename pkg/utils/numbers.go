e90fb00ed8474d1817b881ec79cbc5064d1883fc
winerty <nikitokmay@gmail.com>
2026-06-27 19:48:32 +0300

    Upd(pkg): New utils func for format number

--- pkg/utils/numbers.go
+++ pkg/utils/numbers.go
@@ -1,6 +1,9 @@
 package utils
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func Threshold(val int64) float64 {
 	if val > 1000 {
@@ -11,4 +14,21 @@ func Threshold(val int64) float64 {
 
 func Percent(part, total int64) float64 {
 	return math.Round(float64(part)/float64(total)*10000) / 100
+}
+
+func FormatNumber(val int64) string {
+	// Для отрицательных чисел
+	if val < 0 {
+		return "-" + FormatNumber(-val)
+	}
+
+	switch {
+	case val >= 1_000_000:
+		return fmt.Sprintf("%.1f M", float64(val)/1_000_000.0)
+
+	case val >= 1000:
+		return fmt.Sprintf("%.1f G", float64(val)/1000.0)
+	default:
+		return fmt.Sprintf("%d", val)
+	}
 }
